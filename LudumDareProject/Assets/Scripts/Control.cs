a9ac69e9054058cb24b42aee360e682c7917bcfe
TechEpic <signup1579@gmail.com>
2020-04-18 00:03:59 -0700

    Added drone!

---
+++ LudumDareProject/Assets/Scripts/Control.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Control : MonoBehaviour
+{
+	protected bool isEnabled = true;
+
+	public void enable() {
+		isEnabled = true;
+	}
+
+	public void disable() {
+		isEnabled = false;
+	}
+
+	// Start is called before the first frame update
+	void Start() {
+	}
+
+	void FixedUpdate() {
+	}
+}
---
+++ LudumDareProject/Assets/Scripts/DroneControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DroneControl : Control
+{
+	public string MoveLeft = "a";
+	public string MoveRight = "d";
+	public float MovementForce = 10;
+
+	public GameObject LeftProp;
+	public GameObject RightProp;
+
+	Rigidbody2D rb;
+
+	// Start is called before the first frame update
+	void Start() {
+		rb = GetComponent<Rigidbody2D>();
+		MovementForce *= Time.fixedDeltaTime;
+	}
+
+	void FixedUpdate() {
+		if(!isEnabled) return;
+		Vector2 dir = new Vector2();
+		dir.x = Mathf.Cos(Mathf.Deg2Rad * (transform.eulerAngles.z + 90));
+		dir.y = Mathf.Sin(Mathf.Deg2Rad * (transform.eulerAngles.z + 90));
+		if(Input.GetKey(MoveLeft)) {
+			rb.AddForceAtPosition(dir * MovementForce, RightProp.transform.position);
+		}
+		if(Input.GetKey(MoveRight)) {
+			rb.AddForceAtPosition(dir * MovementForce, LeftProp.transform.position);
+		}
+	}
+}
