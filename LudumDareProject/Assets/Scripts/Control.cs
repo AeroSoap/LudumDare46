e7acdc5ed4526d2809a8be05745b2a410fc58e94
TechEpic <signup1579@gmail.com>
2020-04-18 14:43:49 -0700

    Droning on and on

--- LudumDareProject/Assets/Scripts/Control.cs
+++ LudumDareProject/Assets/Scripts/Control.cs
@@ -5,23 +5,23 @@ using UnityEngine;
 public class Control : MonoBehaviour {
 
 	protected bool isEnabled = false;
+	SpriteRenderer sr;
+
+	void Awake() {
+		sr = GetComponent<SpriteRenderer>();
+	}
 
 	public void enable() {
 		isEnabled = true;
+		sr.color = new Vector4(1, 1, 0.75f, 1);
 	}
 
 	public void disable() {
 		isEnabled = false;
+		sr.color = new Vector4(1, 1, 1, 1);
 	}
 
 	private void OnMouseDown() {
 		Hacker.hack(this);
-	}
-
-	// Start is called before the first frame update
-	void Start() {
-	}
-
-	void FixedUpdate() {
 	}
 }
