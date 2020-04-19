e471f13e72e4d93c5126b40a39a2e7c71e801c03
TechEpic <signup1579@gmail.com>
2020-04-18 23:01:59 -0700

    Added lightning and stormclouds

---
+++ LudumDareProject/Assets/Scripts/StormCloud.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Josh
+
+public class StormCloud : MonoBehaviour
+{
+	public float Interval;
+	public float LightningTime;
+	public GameObject Lightning;
+
+	GameObject lightning;
+
+	// Start is called before the first frame update
+	void Start() {
+		StartCoroutine(LightningSpawn());
+	}
+
+	// Update is called once per frame
+	void Update() {
+		
+	}
+
+	IEnumerator LightningDestroy() {
+		yield return new WaitForSeconds(LightningTime);
+		Destroy(lightning);
+	}
+
+	IEnumerator LightningSpawn() {
+		yield return new WaitForSeconds(Interval);
+		lightning = Instantiate(Lightning, transform.position + new Vector3(0, -50), Quaternion.identity);
+		StartCoroutine(LightningSpawn());
+		StartCoroutine(LightningDestroy());
+	}
+}
